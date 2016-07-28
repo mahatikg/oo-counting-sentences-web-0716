e785bb4c7231c8d86a7597bd739a94ec69adff50
Mahati Gollamudi <github email address>
2016-07-28 14:08:04 -0400

    Utilize Self and Regular Expressions

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,32 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
+    # count = 0
+    # self.split(".") <<< returns an array
+    # this won't work because it fails last test and
 
-  end
+    #   count += 1
+    # elsif self.split("?")
+    #   count += 1
+    # elsif self.split ("!")
+    #   count += 1
+    # else
+    #   return count
+    #binding.pry
+    self.split(/\.+|\!+|\?+/).count
+    end
+
+
 end
